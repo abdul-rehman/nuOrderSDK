fd0e2eb70759b27301ca40272b8323fb85d1854c
Abdul Rehman <abdul.rehman@wearehathway.com>
2016-12-29 16:03:16 +0500

    Initial Commit

---
+++ nuOrderSDK/nuOrderSDK/nuOrderSDK.h
@@ -0,0 +1,19 @@
+//
+//  nuOrderSDK.h
+//  nuOrderSDK
+//
+//  Created by Abdul Rehman on 29/12/2016.
+//  Copyright © 2016 ebricks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for nuOrderSDK.
+FOUNDATION_EXPORT double nuOrderSDKVersionNumber;
+
+//! Project version string for nuOrderSDK.
+FOUNDATION_EXPORT const unsigned char nuOrderSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nuOrderSDK/PublicHeader.h>
+
+
